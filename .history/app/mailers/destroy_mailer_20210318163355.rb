cdaf33e7bc33e2f0df87f2008e1e886abd296bc4
oomiya karen <krn971138@gmail.com>
2021-03-18 16:43:29 +0900

    issues#3実装完了,rubocop確認済

---
+++ .history/app/mailers/destroy_mailer_20210318163355.rb
@@ -0,0 +1,2 @@
+class DestroyMailer < ApplicationMailer
+end
--- app/mailers/leader_change_mailer.rb
+++ app/mailers/leader_change_mailer.rb
@@ -1,7 +1,7 @@
 class LeaderChangeMailer < ApplicationMailer
-    def contact_mail(leaderchange)
-        @team = leaderchange
-        @user = @team.owner
-        mail to: @team.owner.email, subject: "オーナー変更確認メール"
-      end
+  def contact_mail(leaderchange)
+    @team = leaderchange
+    @user = @team.owner
+    mail to: @team.owner.email, subject: "オーナー変更確認メール"
+  end
 end
